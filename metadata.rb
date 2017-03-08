692825ddb1e86a1145d3bb4196788e8704b9b9ce
Jan-Christoph Kuester <jckuester@gmail.com>
2017-03-08 23:39:31 +0100

    Extend metadata
    Description, maintainer, etc.

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,8 @@
 name "lamp-for-aws-cookbook"
+maintainer       'jckuester'
+maintainer_email 'jckuster@gmail.com'
+description      'Installs/Configures Demo LAMP Stack for AWS'
+license          'EPL'
 version "0.1.0"
 
 depends 'apache2', '~> 3.2.2'
