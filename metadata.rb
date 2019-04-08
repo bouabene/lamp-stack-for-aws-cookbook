d8af695cc1e5f6e034083cb7d0195ddd5b3b38d4
k bouabene <k.bouabene@gmail.com>
2019-04-08 10:37:14 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,4 @@
 name "lamp-stack-for-aws-cookbook"
-maintainer       'jckuester'
-maintainer_email 'jckuster@gmail.com'
 description      'Installs and configures Demo LAMP Stack for AWS'
 license          'EPL'
 version 	 '0.1.0'
